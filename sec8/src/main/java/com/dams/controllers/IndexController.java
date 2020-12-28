cd21f2f4744b4841c423234fbc9d9514fc607fc7
Dário Silva <dario.silva@vilt-group.com>
2020-12-28 18:54:09 +0000

    Add Section 8 - Remove unused imports.

--- sec8/src/main/java/com/dams/controllers/IndexController.java
+++ sec8/src/main/java/com/dams/controllers/IndexController.java
@@ -1,15 +1,9 @@
 package com.dams.controllers;
 
-import com.dams.domain.Category;
-import com.dams.domain.UnitOfMeasure;
-import com.dams.repositories.CategoryRepository;
-import com.dams.repositories.UnitOfMeasureRepository;
 import com.dams.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
