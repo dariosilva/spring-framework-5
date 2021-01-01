03f5f6bf094a1ba7a1cd7b31d0ce739b2ce9f187
Dário Silva <dario.silva@vilt-group.com>
2021-01-01 18:21:53 +0000

    Add Section 12 - Validation and Constraints.

---
+++ sec12/src/main/java/com/dams/services/RecipeService.java
@@ -0,0 +1,22 @@
+package com.dams.services;
+
+import com.dams.commands.RecipeCommand;
+import com.dams.domain.Recipe;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+    Recipe findById(Long l);
+
+    RecipeCommand findCommandById(Long l);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+
+    void deleteById(Long idToDelete);
+}
