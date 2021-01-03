82ab9057fd3a4daad50ecb71fcd9847e8657c3f8
Dário Silva <dario.silva@vilt-group.com>
2021-01-03 11:09:03 +0000

    Add Section 14 - Containers and Images

---
+++ sec14/src/main/java/com/dams/services/ProductService.java
@@ -0,0 +1,22 @@
+package com.dams.services;
+
+import com.dams.commands.ProductForm;
+import com.dams.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface ProductService {
+
+    List<Product> listAll();
+
+    Product getById(String id);
+
+    Product saveOrUpdate(Product product);
+
+    void delete(String id);
+
+    Product saveOrUpdateProductForm(ProductForm productForm);
+}
