9f38761fcc41dbe84d662a31dd8d7ef0e9f430cf
Dário Silva <dario.silva@vilt-group.com>
2021-01-04 23:15:05 +0000

    Add Section 19 - Spring Data MongoDB.

---
+++ sec19/src/main/java/com/dams/services/RecipeService.java
@@ -0,0 +1,22 @@
+package com.dams.services;
+
+import com.dams.domain.Recipe;
+import com.dams.commands.RecipeCommand;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeService {
+
+    Set<Recipe> getRecipes();
+
+    Recipe findById(String id);
+
+    RecipeCommand findCommandById(String id);
+
+    RecipeCommand saveRecipeCommand(RecipeCommand command);
+
+    void deleteById(String idToDelete);
+}
---
+++ sec19/src/main/java/com/dams/services/UnitOfMeasureService.java
@@ -0,0 +1,13 @@
+package com.dams.services;
+
+import com.dams.commands.UnitOfMeasureCommand;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 6/28/17.
+ */
+public interface UnitOfMeasureService {
+
+    Set<UnitOfMeasureCommand> listAllUoms();
+}
