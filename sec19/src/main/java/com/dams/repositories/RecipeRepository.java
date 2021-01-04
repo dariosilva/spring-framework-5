c08f0374bb975b8cc8d2492f5d61f1d9425206e6
Dário Silva <dario.silva@vilt-group.com>
2021-01-04 23:15:05 +0000

    Add sec19 - Spring Data MongoDB.

---
+++ sec19/src/main/java/com/dams/repositories/RecipeRepository.java
@@ -0,0 +1,10 @@
+package com.dams.repositories;
+
+import com.dams.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public interface RecipeRepository extends CrudRepository<Recipe, String> {
+}
---
+++ sec19/src/main/java/com/dams/services/IngredientService.java
@@ -0,0 +1,15 @@
+package com.dams.services;
+
+import com.dams.commands.IngredientCommand;
+
+/**
+ * Created by jt on 6/27/17.
+ */
+public interface IngredientService {
+
+    IngredientCommand findByRecipeIdAndIngredientId(String recipeId, String ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
+
+    void deleteById(String recipeId, String idToDelete);
+}
